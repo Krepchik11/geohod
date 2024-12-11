c52ca67aaeb9f74ad3b9df6f9935923e0c2c1d20
pavel.romanov <pavel.romanov@demlabs.net>
2024-12-11 17:53:34 +0100

    fixed baseURL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: '',
   headers: {
     'Content-Type': 'application/json',
     // 'Authorization': `Bearer 7686811149:AAG0SNhXi5SGRQF15yzq4bfmfcRDhbY03uU`  Сергея
