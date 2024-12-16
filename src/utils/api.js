97de6d23aa8a6cd67f7aa3be9f6bfcb992efbda2
pavel.romanov <pavel.romanov@demlabs.net>
2024-12-16 19:35:10 +0100

    Integrated with the backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,57 +1,35 @@
+import { retrieveLaunchParams } from '@telegram-apps/sdk';
 import axios from 'axios';
-import CryptoJS from 'crypto-js';
 
-function validateInitData(initData, botToken) {
-  const urlSearchParams = new URLSearchParams(initData);
-  const data = Object.fromEntries(urlSearchParams.entries());
+const { initDataRaw } = retrieveLaunchParams();
 
-  console.log('data:', data);
-  
-  const checkString = Object.keys(data)
-    .filter((key) => key !== 'hash')
-    .sort()
-    .map((key) => `${key}=${data[key]}`)
-    .join('\n');
+// fetch('https://example.com/api/authorize', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Authorization': `tma ${initDataRaw}`
+//   },
+// })
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log('Authorization success:', data);
+//   })
+//   .catch(error => {
+//     console.error('Authorization failed:', error);
+//   });
 
+console.log('initDataRaw:', initDataRaw);
 
-  const signature = CryptoJS.HmacSHA256(checkString, botToken).toString(CryptoJS.enc.Hex);
-
-  console.log('checkString:', checkString);
-  console.log('signature:', signature);
-  console.log('data.hash:', data.hash);
-
-  const isValid = data.hash === signature;
-
-  console.log('Совпадают: ', isValid);
-
-  return isValid;
-}
-
-
-
-const initData = window.Telegram.WebApp.initData;
-const botToken = '7743241154:AAHtg7ipMzF8DjRnZQTBtR49uUIDj3zBe1w';
-
-if (!initData) {
-  console.error('initData отсутствует в URL');
-  throw new Error('initData отсутствует');
-}
-
-if (!validateInitData(initData, botToken)) {
-  console.error('Ошибка: initData не прошло проверку!');
-  throw new Error('Неверные данные инициализации!');
-}
-
-console.log('initData успешно проверено!');
 
 const api = axios.create({
   baseURL: '', 
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${initData}`, 
+    Authorization: `tma ${initDataRaw}`, 
   },
 });
 
+console.log('api:', api);
 
 export const get = async (url) => {
   try {
@@ -74,6 +52,89 @@ export const post = async (url, data) => {
     throw error;
   }
 };
+
+
+
+
+
+
+
+// import axios from 'axios';
+// import CryptoJS from 'crypto-js';
+
+// function validateInitData(initData, botToken) {
+//   const urlSearchParams = new URLSearchParams(initData);
+//   const data = Object.fromEntries(urlSearchParams.entries());
+
+//   console.log('data:', data);
+
+//   const checkString = Object.keys(data)
+//     .filter((key) => key !== 'hash')
+//     .sort()
+//     .map((key) => `${key}=${data[key]}`)
+//     .join('\n');
+
+
+//   const signature = CryptoJS.HmacSHA256(checkString, botToken).toString(CryptoJS.enc.Hex);
+
+//   console.log('checkString:', checkString);
+//   console.log('signature:', signature);
+//   console.log('data.hash:', data.hash);
+
+//   const isValid = data.hash === signature;
+
+//   console.log('Совпадают: ', isValid);
+
+//   return isValid;
+// }
+
+
+
+// const initData = window.Telegram.WebApp.initData;
+// const botToken = '7743241154:AAHtg7ipMzF8DjRnZQTBtR49uUIDj3zBe1w';
+
+// if (!initData) {
+//   console.error('initData отсутствует в URL');
+//   throw new Error('initData отсутствует');
+// }
+
+// if (!validateInitData(initData, botToken)) {
+//   console.error('Ошибка: initData не прошло проверку!');
+//   throw new Error('Неверные данные инициализации!');
+// }
+
+// console.log('initData успешно проверено!');
+
+// const api = axios.create({
+//   baseURL: '', 
+//   headers: {
+//     'Content-Type': 'application/json',
+//     Authorization: `Bearer ${initData}`, 
+//   },
+// });
+
+
+// export const get = async (url) => {
+//   try {
+//     const response = await api.get(url);
+//     console.log('response:', response.data);
+//     return response.data;
+//   } catch (error) {
+//     console.error('Ошибка при выполнении GET-запроса:', error.response || error);
+//     throw error;
+//   }
+// };
+
+
+// export const post = async (url, data) => {
+//   try {
+//     const response = await api.post(url, data);
+//     return response.data;
+//   } catch (error) {
+//     console.error('Ошибка при выполнении POST-запроса:', error.response || error);
+//     throw error;
+//   }
+// };
 
 
 
