c8085d4db6c3481408b06a6dac1d5adfa75fee63
pavel.romanov <pavel.romanov@demlabs.net>
2024-12-25 13:23:53 +0100

    Integrated with the backend

--- src/stores/eventStore.js
+++ src/stores/eventStore.js
@@ -4,13 +4,18 @@ import { get } from '../utils/api'
 export const useEventStore = defineStore( 'eventStore', {
   state: () => ({
     events: [],
-    disabledEventId: null,
   }),
   actions: {
     async fetchEvents() {
       try {
         const data = await get( '/api/v1/events' )
-        this.events = Array.isArray( data.content ) ? data.content : []
+
+        // Фильтрация только активных событий
+        const activeEvents = Array.isArray( data.content )
+          ? data.content.filter( event => event.status === 'ACTIVE' )
+          : []
+
+        this.events = activeEvents
         console.log( 'data:', data )
         
       } catch ( error ) {
@@ -21,46 +26,3 @@ export const useEventStore = defineStore( 'eventStore', {
     },    
   },
 })
-
-// export const useEventStore = defineStore( 'event', {
-//   state: () => ({
-//     events: [
-//     ],  // Массив мероприятий, который будет храниться в памяти
-//     disabledEventId: null, // ID события, для которого кнопка заблокирована
-//   }),
-//   actions: {
-//     addEvent( event ) {
-//       event.id = Date.now() // Генерация ID для события
-//       event.currentParticipants = event.currentParticipants ?? 0
-//       event.maxParticipants = event.maxParticipants ?? 0
-//       this.events.push( event ) // Добавляем мероприятие в список     
-//     },
-//     updateEvent( updatedEvent ) {
-//       const index = this.events.findIndex( event => event.id === updatedEvent.id )
-//       if ( index !== -1 ) {
-//         this.events[ index ] = updatedEvent
-//       }
-//     },
-//     getEventById( id ) {
-//       return this.events.find( event => event.id === id ) // Находим событие по ID
-//     },
-//     setDisabledEventId( id ) {
-//       this.disabledEventId = id // Устанавливаем ID заблокированного события
-//     },
-//     incrementParticipants( id ) {
-//       const event = this.getEventById( id )
-//       if ( event && event.currentParticipants < event.maxParticipants ) {
-//         event.currentParticipants++
-//       }
-//     },
-//     decrementParticipants( id ) {
-//       const event = this.getEventById( id )
-//       if ( event && event.currentParticipants > 0 ) {
-//         event.currentParticipants--
-//       }
-//     },
-//     deleteEvent( id ) {
-//       this.events = this.events.filter( event => event.id !== id ) // Удаление события
-//     },
-//   },
-// })
