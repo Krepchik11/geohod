9cf4dcc46d18f6eb4ac7bed9cfb3b9a458b44119
pavel.romanov <pavel.romanov@demlabs.net>
2024-12-25 12:36:35 +0100

    Integrated with the backend

--- src/views/DeleteEventView.vue
+++ src/views/DeleteEventView.vue
@@ -2,7 +2,7 @@
 import { useRoute, useRouter } from 'vue-router'
 import { computed, ref } from 'vue'
 import { MainButton } from 'vue-tg'
-import { deleteRequest } from '../utils/api'
+import { get, deleteRequest } from '../utils/api'
 
 import Header from '../components/Header.vue'
 import { useEventStore } from '../stores/eventStore'
@@ -15,9 +15,11 @@ const isLoading = ref( false )
 
 const eventId = computed( () => route.params.id )
 
-const selectedEvent = computed(() =>
-  eventStore.events.find( event => event.id === eventId.value ) 
-)
+const name = ref( '' )
+const description = ref( '' )
+const date = ref( new Date() )
+const currentParticipants = ref( 0 )
+const maxParticipants = ref( null )
 
 function formattedDate( date ) {
   if ( !date ) return ''
@@ -28,33 +30,33 @@ function formattedDate( date ) {
   }).format( new Date( date ) )
 }
 
-// async function loadEvent() {
-//   try {
-//     if ( !eventId.value ) throw new Error( 'ID мероприятия отсутствует.' )
+async function loadEvent() {
+  try {
+    if ( !eventId.value ) throw new Error( 'ID мероприятия отсутствует.' )
 
-//     const localEvent = eventStore.events.find( event => event.id === eventId.value )
+    const localEvent = eventStore.events.find( event => event.id === eventId.value )
 
-//     if ( localEvent ) {
-//       name.value = localEvent.description
-//       description.value = localEvent.description
-//       date.value = new Date( localEvent.date )
-//       maxParticipants.value = localEvent.maxParticipants
-//       currentParticipants.value = localEvent.currentParticipants || 0
-//     } else {
-//       const data = await get( `/api/v1/events/${ eventId.value }` )
-//       if ( !data ) throw new Error( 'Событие не найдено.' )
+    if ( localEvent ) {
+      name.value = localEvent.description
+      description.value = localEvent.description
+      date.value = new Date( localEvent.date )
+      maxParticipants.value = localEvent.maxParticipants
+      currentParticipants.value = localEvent.currentParticipants || 0
+    } else {
+      const data = await get( `/api/v1/events/${ eventId.value }` )
+      if ( !data ) throw new Error( 'Событие не найдено.' )
 
-//       name.value = data.description
-//       description.value = data.description
-//       date.value = new Date( data.date) 
-//       maxParticipants.value = data.maxParticipants
-//       currentParticipants.value = data.currentParticipants || 0
-//     }
-//   } catch ( error ) {
-//     console.error( 'Ошибка загрузки данных мероприятия:', error )
-//     router.push( '/' )
-//   }
-// }
+      name.value = data.description
+      description.value = data.description
+      date.value = new Date( data.date) 
+      maxParticipants.value = data.maxParticipants
+      currentParticipants.value = data.currentParticipants || 0
+    }
+  } catch ( error ) {
+    console.error( 'Ошибка загрузки данных мероприятия:', error )
+    router.push( '/' )
+  }
+}
 
 async function deleteEvent() {
   try {
@@ -68,25 +70,25 @@ async function deleteEvent() {
   }
 }
 
-// loadEvent()
+loadEvent()
 
 </script>
 
 <template>
   <div class="delete-section">
     <Header>Удаление мероприятия</Header>
-    <div class="delete-section__content" v-if="selectedEvent">
-      <h3 class="delete-section__title">{{ selectedEvent.description }}</h3>
+    <div class="delete-section__content" v-if="name">
+      <h3 class="delete-section__title">{{ name }}</h3>
       <div class="delete-section__details">
-        <p class="delete-section__date">{{ formattedDate( selectedEvent.date ) }}</p>
+        <p class="delete-section__date">{{ formattedDate( date ) }}</p>
       </div>
       <div class="delete-section__members-list"></div>
       <div class="delete-section__members-quantity">
-        <p class="delete-section__members-registered">{{ selectedEvent.currentParticipants }}</p>
+        <p class="delete-section__members-registered">{{ currentParticipants }}</p>
         из
-        <p class="delete-section__members-all">{{ selectedEvent.maxParticipants }}</p>
+        <p class="delete-section__members-all">{{ maxParticipants }}</p>
       </div>
-      <Message v-if="0 < selectedEvent.currentParticipants">На мероприятие записаны люди!</Message>
+      <Message v-if="currentParticipants > 0">На мероприятие записаны люди!</Message>
       <MainButton text="Удалить" @click="deleteEvent" />
     </div>
 
