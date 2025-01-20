72a427964c19f003230e1d8f906b168060c76373
pavel.romanov <pavel.romanov@demlabs.net>
2025-01-20 13:54:00 +0100

    added redirection to the main page if the user is registered

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,8 @@ import DeleteEventView from '../views/DeleteEventView.vue'
 import ParticipantListView from '../views/ParticipantListView.vue'
 import FinishEventView from '../views/FinishEventView.vue'
 
+import { get } from '../utils/api'
+
 const router = createRouter({
   history: createWebHistory( import.meta.env.BASE_URL ),
   routes: [
@@ -30,6 +32,43 @@ const router = createRouter({
       name: 'registration', 
       component: () => import('../views/RegistrationView.vue'), 
       props: true, 
+      beforeEnter: async ( to, from, next ) => {
+        const initData = window.Telegram.WebApp.initData || ''
+        const decodedInitData = decodeURIComponent( initData )
+        const params = new URLSearchParams( decodedInitData )
+        const userParam = params.get( 'user' )
+
+        let extractedUsername = null
+        if ( userParam ) {
+          const user = JSON.parse( userParam )
+          extractedUsername = user.username
+        }
+
+        if ( !extractedUsername ) {
+          console.error( 'Не удалось извлечь имя пользователя из initData.' )
+          return next( '/' ) // Перенаправляем на главную страницу
+        }
+
+        const eventId = to.params.id
+        try {
+          const data = await get( `/api/v1/events/${ eventId }/participants` )
+          if (!data || !data.participants) throw new Error( 'Участники не найдены.' )
+
+          const isRegistred = data.participants.some(
+            ( participant ) => participant.username === extractedUsername
+          )
+
+          if ( isRegistred ) {
+            // Если пользователь зарегистрирован, перенаправляем на главную страницу
+            return next( '/' )
+          }
+        } catch ( error ) {
+          console.error( 'Ошибка загрузки участников:', error )
+        }
+
+        // Если пользователь не зарегистрирован, продолжаем переход к странице регистрации
+        next()
+      },
     },
     {
       path: '/participants/:id',
