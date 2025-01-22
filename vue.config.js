68af2c4ca094d4929cf69e28a9ae69d588395683
pavel.romanov <pavel.romanov@demlabs.net>
2025-01-22 09:04:13 +0100

    changed the url of the backend

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://45.91.92.11',  // Адрес API
+        target: 'http://45.91.92.11:8080',  // Адрес API
         changeOrigin: true, 
         secure: false,  // Для работы с http 
         pathRewrite: {
